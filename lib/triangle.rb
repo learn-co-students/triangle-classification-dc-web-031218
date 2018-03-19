9ccb759e5018a9a6773ed76b7799974817781523
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-19 11:51:36 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
+
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def valid?
+    if(@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+      false
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side3 + @side2 <= @side1)
+      false
+    else
+      true
+    end
+  end
+
+  def kind
+    #binding.pry
+    if !self.valid? #may not need self
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif (@side1 == @side2 && @side2 != @side3) || (@side1 == @side3 && @side2 != @side3) || (@side3 == @side2 && @side2 != @side1)
+      #binding.pry
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
