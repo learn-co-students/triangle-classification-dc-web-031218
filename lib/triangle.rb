99b20d6978200168b164587796f8e9efd08a42ff
azeelleeza <leezaluncheon@gmail.com>
2018-03-20 17:43:31 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require "pry"
+
 class Triangle
-  # write code here
+ attr_reader :kind,:args
+
+ def initialize(*args)
+   @args = args
+
+   if invalid?
+     begin
+       raise TriangleError
+     end
+    elsif args.uniq.length == 1
+      @kind = :equilateral
+    elsif args.uniq.length == 2
+      @kind = :isosceles
+    elsif args.uniq.length == 3
+      @kind = :scalene
+    else
+    end
+
+  end
+end
+
+def invalid?
+  args.include?(0) || args[0] + args[1] <= args[2] || args[1] + args[2] <= args[0] || args[0] + args[2] <= args[1]
+end
+
+
+class TriangleError < StandardError
+    # def message
+    #   "not a triangle"
+    # end
 end
